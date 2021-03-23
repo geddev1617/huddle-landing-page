243f3424f3c185f4eec3af11e66740f8caf239c7
Toby Okeke <toby.okeke@gmail.com>
2021-03-23 14:13:30 +0100

    created routers and added assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingpageComponent},
+  {path: 'navbar', component: NavbarComponent},
+  {path: 'footer', component: FooterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
